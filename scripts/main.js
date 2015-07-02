36c6efb5c7c5a18da77935fc1f6d4bed313e702b
Konrad Dzwinel <kdzwinel@gmail.com>
2015-07-03 01:50:24 +0200

    Support for Domain URLs.

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('WebComponentsReady', function () {
+  var viewerApp = document.querySelector('viewer-app');
+  viewerApp.setProtocolSchema(protocol);
+
+  /*
+   ROUTING
+   */
+
+  page('/', function () {
+    viewerApp.setDomainByName(null);
+  });
+
+  page('/:domain', function (data) {
+    viewerApp.setDomainByName(data.params && data.params.domain);
+  });
+
+  // add #! before urls
+  page({
+    hashbang: true
+  });
+});
