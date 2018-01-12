d388c240ba016322b907657c0afa5e7e0800c046
Paul Irish <paul.irish@gmail.com>
2018-01-12 12:58:33 -0800

    update script: simplify update & deployment

--- scripts/update-n-publish-docs.sh
+++ scripts/update-n-publish-docs.sh
@@ -6,17 +6,14 @@
 set -x
 
 # These locations are very machine specific.
-protocol_repo_path="$HOME/code/pristine/devtools-protocol"
 viewer_repo_path="$HOME/code/pristine/debugger-protocol-viewer-pristine"
 local_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$viewer_repo_path"
-git remote add dtprotocol git@github.com:ChromeDevTools/devtools-protocol.git
+cd "$viewer_repo_path" || exit
+viewer_repo="origin master"
 
 git checkout master
-git pull origin master
-# first, publish any changes we got from the source repo over to gh-pages
-git push dtprotocol master:gh-pages
+git pull $viewer_repo
 
 # generate latest docs on updated protocol
 bower install
@@ -26,8 +23,8 @@ git commit --author="DevTools Bot <paulirish+bot@google.com>" -am "bump protocol
 #    git config user.name "devtools-bot"
 #    git config user.email "paulirish+bot@google.com"
 
-# push changes back to source repo
-git pull origin master && git push origin master
+# push protocol bump commit back to viewer repo
+git pull $viewer_repo && git push $viewer_repo
 
 # publish to https://chromedevtools.github.io/devtools-protocol/
-git pull origin master && yarn deploy
+yarn deploy
