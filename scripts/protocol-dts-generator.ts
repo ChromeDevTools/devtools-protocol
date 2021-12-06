ff0f13c4cb31a21a11650b3c3c918fc5c53414b5
Brandon Walderman <bwalderman@outlook.com>
2021-12-05 22:49:22 -0800

    Revert changes.

--- scripts/protocol-dts-generator.ts
+++ scripts/protocol-dts-generator.ts
@@ -287,10 +287,10 @@ const getCommandMapping = (command: P.Command, domainName: string, modulePrefix:
 
 const flatten = <T>(arr: T[][]) => ([] as T[]).concat(...arr)
 
-const emitMapping = (moduleName: string, protocolModuleName: string, domains: P.Domain[], moduleType: ModuleType) => {
+const emitMapping = (moduleName: string, protocolModuleName: string, domains: P.Domain[]) => {
     moduleName = toTitleCase(moduleName)
     emitHeaderComments()
-    emitProtocolImport(protocolModuleName, moduleType)
+    emitLine(`import Protocol from './${protocolModuleName}'`)
     emitLine()
     emitDescription('Mappings from protocol event and command names to the types required for them.')
     emitOpenBlock(`export namespace ${moduleName}`)
@@ -341,16 +341,10 @@ const emitDomainApi = (domain: P.Domain, modulePrefix: string) => {
     emitCloseBlock()
 }
 
-const emitProtocolImport = (protocolModuleName: string, moduleType: ModuleType) => {
-    let protocolModuleImportPath = `./${protocolModuleName}`;
-    if (moduleType === "esm") protocolModuleImportPath += ".d.ts"
-    emitLine(`import Protocol from '${protocolModuleImportPath}'`)
-}
-
-const emitApi = (moduleName: string, protocolModuleName: string, domains: P.Domain[], moduleType: ModuleType) => {
+const emitApi = (moduleName: string, protocolModuleName: string, domains: P.Domain[]) => {
     moduleName = toTitleCase(moduleName)
     emitHeaderComments()
-    emitProtocolImport(protocolModuleName, moduleType)
+    emitLine(`import Protocol from './${protocolModuleName}'`)
     emitLine()
     emitDescription('API generated from Protocol commands and events.')
     emitOpenBlock(`export namespace ${moduleName}`)
@@ -380,23 +374,18 @@ const flushEmitToFile = (path: string) => {
     emitStr = ''
 }
 
-type ModuleType = "commonjs" | "esm";
-
 // Main
-const moduleType = process.argv[2] as ModuleType;
-const destModuleDir: string = process.argv[3];
-
-const destProtocolFilePath = `${__dirname}/../${destModuleDir}/protocol.d.ts`
+const destProtocolFilePath = `${__dirname}/../types/protocol.d.ts`
 const protocolModuleName = path.basename(destProtocolFilePath, '.d.ts')
 emitModule(protocolModuleName, protocolDomains)
 flushEmitToFile(destProtocolFilePath)
 
-const destMappingFilePath = `${__dirname}/../${destModuleDir}/protocol-mapping.d.ts`
+const destMappingFilePath = `${__dirname}/../types/protocol-mapping.d.ts`
 const mappingModuleName = 'ProtocolMapping'
-emitMapping(mappingModuleName, protocolModuleName, protocolDomains, moduleType)
+emitMapping(mappingModuleName, protocolModuleName, protocolDomains)
 flushEmitToFile(destMappingFilePath)
 
-const destApiFilePath = `${__dirname}/../${destModuleDir}/protocol-proxy-api.d.ts`
+const destApiFilePath = `${__dirname}/../types/protocol-proxy-api.d.ts`
 const apiModuleName = 'ProtocolProxyApi'
-emitApi(apiModuleName, protocolModuleName, protocolDomains, moduleType)
+emitApi(apiModuleName, protocolModuleName, protocolDomains)
 flushEmitToFile(destApiFilePath)
