c5298b2e135d3cc38fc6593b332fa3fe36624d0f
Vlad Dmitrievich <2tunnels@gmail.com>
2025-02-01 17:42:05 +0200

    Update loaderId description for requestWillBeSent and responseReceived network events

--- types/protocol.d.ts
+++ types/protocol.d.ts
@@ -12267,7 +12267,7 @@ export namespace Protocol {
              */
             requestId: RequestId;
             /**
-             * Loader identifier. Empty string if the request is fetched from worker.
+             * Loader identifier. Empty string for a preflight request or if the request is fetched from a worker.
              */
             loaderId: LoaderId;
             /**
@@ -12355,7 +12355,7 @@ export namespace Protocol {
              */
             requestId: RequestId;
             /**
-             * Loader identifier. Empty string if the request is fetched from worker.
+             * Loader identifier. Empty string for a preflight request or if the request is fetched from a worker.
              */
             loaderId: LoaderId;
             /**
